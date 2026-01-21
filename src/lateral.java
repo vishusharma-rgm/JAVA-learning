ee391e0f91795dbc287afe90d192dc79ccc07625
vishusharma <sikaridon9@gmail.com>
2026-01-21 15:03:51 +0530

    lateral

---
+++ src/lateral.java
@@ -0,0 +1,27 @@
+public class lateral {
+    public static void main(String[] args) {
+        int age = 22;
+        byte age2 = 44;
+        short age3 = 21;
+        long ageDino = 77;
+        char c1 = 'a';
+        float f1 = 99.7f;
+        double d1 = 33.89;
+        boolean a = true;
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(ageDino);
+        System.out.println(c1);
+        System.out.println(f1);
+        System.out.println(d1);
+
+        System.out.println("Thaynk you :");
+
+
+
+
+
+
+    }
+}
