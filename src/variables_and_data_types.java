87b21a9f0e2ed08ee60aa0585e67be219d0be19f
vishusharma <sikaridon9@gmail.com>
2026-01-21 14:46:32 +0530

    variables and data types

---
+++ src/variables_and_data_types.java
@@ -0,0 +1,14 @@
+public class variables_and_data_types {
+    public static void main(String[] args) {
+        System.out.println("the sum of these num is :");
+        int num1 = 33;
+        int num2 = 23;
+        int num3 = 13;
+        int num4 = 43;
+        int sum = num1+ num2+ num3+ num4;
+        System.out.println(sum);
+
+
+
+    }
+}
