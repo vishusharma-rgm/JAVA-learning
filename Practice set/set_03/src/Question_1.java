23386ece632bdbfcaf541c0971dc76e6674b847b
vishusharma <sikaridon9@gmail.com>
2026-01-22 23:49:55 +0530

    practice set

---
+++ Practice set/set_03/src/Question_1.java
@@ -0,0 +1,17 @@
+
+// Question 1. Write a program to sum of three number
+
+public class Question_1 {
+    public static void main(String[] args) {
+
+        int a = 11;
+        int b = 22;
+        int c = 77;
+        int sum = a+b+c;
+
+        System.out.println(sum );
+        System.out.println("Thanks ");
+
+
+    }
+}
