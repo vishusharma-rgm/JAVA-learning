a192e81d41eb501bfe9dedf237b8e213603107b9
vishusharma <sikaridon9@gmail.com>
2026-01-25 21:14:48 +0530

    Questions

---
+++ Practice set/set_01/Q_02.java
@@ -0,0 +1,17 @@
+// Q_02 Write program to calculate CGPA Using marks of three subject oit of 100
+
+public class Q_02 {
+    public static void main(String[] args) {
+        float subject1 = 34;
+        float subject2 = 54;
+        float subject3 = 14;
+        float subject4 = 94;
+        float cgpa = (subject1 + subject2 + subject3 + subject4) / 40;
+        System.out.println(cgpa);
+    }
+
+
+}
+
+
+
