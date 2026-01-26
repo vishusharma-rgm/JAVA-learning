06f1d32d97b34cb4663fb1aabc4941a5695ebecc
vishusharma <sikaridon9@gmail.com>
2026-01-26 21:37:54 +0530

    Questions

---
+++ Practice set/set_01/Q_03.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q_03 {
+    public static void main(String[] args) {
+        System.out.println(("what is your name"));
+        Scanner sc = new Scanner(System.in);
+        String name =sc.next();
+        System.out.println("hello " + name + " have a good day !");
+
+
+
+
+    }
+}
