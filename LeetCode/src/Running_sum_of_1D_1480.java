b8233e25b78acd6d88c386db3299b53014aa2509
vishusharma <sikaridon9@gmail.com>
2026-01-28 23:45:24 +0530

    Questions

---
+++ LeetCode/src/Running_sum_of_1D_1480.java
@@ -0,0 +1,16 @@
+public class Running_sum_of_1D_1480 {
+    class Solution {
+        public int[] runningSum(int[] nums) {
+            int sum = 0;
+            for(int i =0; i<nums.length; i++){
+                sum+= nums[i];
+                nums[i]= sum;
+
+            }
+
+            return nums;
+        }
+    }
+
+
+}
