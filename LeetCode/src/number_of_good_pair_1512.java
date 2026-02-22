e0656d7578b4d2e09f9a6232456886049a2d4d22
vishusharma <sikaridon9@gmail.com>
2026-02-22 23:16:35 +0530

    1512

--- LeetCode/src/number_of_good_pair_1512.java
+++ LeetCode/src/number_of_good_pair_1512.java
@@ -1,12 +1,25 @@
+// class Solution {
+//     public int numIdenticalPairs(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int count = 0;
+
+//         for (int num : nums) {
+//             int freq = map.getOrDefault(num, 0);
+//             count += freq;   // existing same numbers form pairs
+//             map.put(num, freq + 1);
+//         }
+
+//         return count;
+//     }
+// }
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] freq = new int[101];   // because 1 <= nums[i] <= 100
         int count = 0;
 
         for (int num : nums) {
-            int freq = map.getOrDefault(num, 0);
-            count += freq;   // existing same numbers form pairs
-            map.put(num, freq + 1);
+            count += freq[num];   // existing same numbers form pairs
+            freq[num]++;
         }
 
         return count;
