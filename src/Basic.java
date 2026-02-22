4b9af3eed506cf8374398c820c0203462ce3f55e
vishusharma <sikaridon9@gmail.com>
2026-02-23 01:17:17 +0530

    Java-learning

---
+++ src/Basic.java
@@ -0,0 +1,23 @@
+public class Basic {
+    public static void main(String[] args) {
+    //    int a = 10;
+    //     if(a==10){
+    //     System.out.println("Hello World");
+
+    //    }
+
+    //    int count = 1;
+    //    while(count !=5){
+    //     System.out.println(count);
+    //     count++;
+    //    }
+
+    for(int count =1; count!=5; count++){
+        System.out.println(count);
+        count++;
+        
+    }
+    }
+
+    
+}
---
+++ src/typeCasting.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class typeCasting {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // float num = input.nextFloat();
+
+        // typeCasting
+        int num = (int) (67.66f);
+        System.out.println(num);
+        
+
+
+
+
+    }
+    
+}
