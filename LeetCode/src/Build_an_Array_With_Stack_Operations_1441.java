8a35b26b6d18f00315905279f82fa97acd931d61
vishusharma <sikaridon9@gmail.com>
2026-02-25 22:49:17 +0530

    Build_an_Array_With_Stack_Operation

---
+++ LeetCode/src/Build_an_Array_With_Stack_Operations_1441.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+
+        int j = 0; // pointer for target
+
+        for (int i = 1; i <= n && j < target.length; i++) {
+            result.add("Push");
+
+            if (i == target[j]) {
+                j++;
+            } else {
+                result.add("Pop");
+            }
+        }
+
+        return result;
+    }
+}
