9bd0dab2f4835b3288f8bad4af7996da07954164
vishusharma <sikaridon9@gmail.com>
2026-02-27 23:59:35 +0530

    scope

---
+++ src/scope.java
@@ -0,0 +1,15 @@
+public class scope {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // System.out.println(marks);
+            
+    }
+    static void random(int marks){
+        int num = 100;
+        System.out.println(marks);
+        System.out.println(num);
+    }
+    
+}
