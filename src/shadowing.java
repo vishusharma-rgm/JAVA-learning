d5e5c601fa2bab871837201dea62c8d68a907569
vishusharma <sikaridon9@gmail.com>
2026-03-01 22:13:35 +0530

    shadowing

---
+++ src/shadowing.java
@@ -0,0 +1,15 @@
+public class shadowing {
+    static int x = 10;
+    public static void main(String[] args) {
+        System.out.println(x);
+        int x = 40;
+        System.out.println(x);
+        
+        fun();
+    }
+    static void fun(){
+        System.out.println(x);
+    }
+
+    
+}
