56a67d4eded089e351fdd495afc1e2b0432a1144
vishusharma <sikaridon9@gmail.com>
2026-03-03 18:22:01 +0530

    Overloading

---
+++ src/Overloading.java
@@ -0,0 +1,18 @@
+public class Overloading {
+    public static void main(String[] args) {
+        fun(34);
+        fun("vishu");
+
+    }
+    static void fun(int a){
+        System.out.println(a);
+
+
+    }
+    static void fun(String name){
+        System.out.println(name);
+
+       
+    }
+
+}
