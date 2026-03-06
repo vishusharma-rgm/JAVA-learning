13d3568da86edb03aedc94335c49060d3c833b6b
vishusharma <sikaridon9@gmail.com>
2026-03-06 18:35:18 +0530

    basicArray

--- src/Basic.java
+++ src/Basic.java
@@ -12,11 +12,17 @@ public class Basic {
     //     count++;
     //    }
 
+    // for(int count =1; count!=5; count++){
+    //     System.out.println(count);
+    //     count++;
+
+
     for(int count =1; count!=5; count++){
         System.out.println(count);
         count++;
         
     }
+    
     }
 
     
