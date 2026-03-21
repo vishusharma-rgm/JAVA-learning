bdff95ca1d0c1b696441447dab1e22165ab65795
vishusharma <sikaridon9@gmail.com>
2026-03-21 19:56:03 +0530

    SearchInString

---
+++ src/LinearSearch/SearchInString.java
@@ -0,0 +1,43 @@
+package LinearSearch;
+
+import java.util.Arrays;
+
+public class SearchInString {
+    public static void main(String[] args) {
+        String name ="vishu";
+        char target = 'u';
+       
+        System.out.println(Arrays.toString(name.toCharArray()));
+        
+    }
+    static boolean search2(String str, char target){
+        if (str.length()==0){
+           return false;
+
+        }
+            for(char ch :str.toCharArray()){
+                if (ch==target) {
+                    return true;
+
+                    
+                }
+            }
+            return false;
+
+            }
+
+
+
+    static boolean search(String str, char target){
+        if (str.length()==0){
+           return false;
+
+        }
+            for (int i = 0; i < str.length(); i++) {
+                if (target == str.charAt(i)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
