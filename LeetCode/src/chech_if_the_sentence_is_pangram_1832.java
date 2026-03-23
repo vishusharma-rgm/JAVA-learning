06a188a571f201bfb9ab83a8dc50f0e458d01c17
vishusharma <sikaridon9@gmail.com>
2026-03-23 23:59:15 +0530

    LeetCode/src/chech_if_the_sentence_is_pangram_1832.java

---
+++ LeetCode/src/chech_if_the_sentence_is_pangram_1832.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/check-if-the-sentence-is-pangram/submissions/1957045658
+class Solution{
+public boolean checkIfPangram(String sentence) {
+        
+        boolean []seen = new boolean[26];
+        for(int i = 0; i<sentence.length();i++){
+            char ch = sentence.charAt(i);
+            int index=ch-'a';
+            seen[index]=true;
+        }
+        for(int i=0; i<26; i++){
+            if(seen[i]==false){
+                return false;
+            }
+        }
+        return true;
+        }
+    }
