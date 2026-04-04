288185a119590911ffaa081c2253d65e59c9b357
vishusharma <sikaridon9@gmail.com>
2026-04-04 21:11:57 +0530

    square_63.java

---
+++ LeetCode/src/square_63.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int mySqrt(int x) {
+
+        int left = 0;
+        int right = x;
+
+        while(left <= right){
+
+            int mid = left + (right-left)/2;
+
+            long sq = (long) mid * mid;
+
+            if(sq == x){
+                return mid;
+            }
+
+            else if(sq < x){
+                left = mid + 1;
+            }
+
+            else{
+                right = mid - 1;
+            }
+        }
+
+        return right;
+    }
+}
