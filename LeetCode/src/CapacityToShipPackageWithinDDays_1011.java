0a05c9ed324f1e81282dda70500af956ac3f3194
vishusharma <sikaridon9@gmail.com>
2026-04-07 00:02:40 +0530

    CapacityToShipPackageWithinDDays_1011.java

---
+++ LeetCode/src/CapacityToShipPackageWithinDDays_1011.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+    public int shipWithinDays(int[] weights, int days) {
+        
+        int low = 0;
+        int high = 0;
+        
+        for(int w : weights){
+            low = Math.max(low, w);
+            high += w;
+        }
+        
+        while(low <= high){
+            
+            int mid = low + (high-low)/2;
+            
+            if(canShip(weights, days, mid)){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        return low;
+    }
+    
+    
+    private boolean canShip(int[] weights, int days, int capacity){
+        
+        int usedDays = 1;
+        int currentLoad = 0;
+        
+        for(int w : weights){
+            
+            if(currentLoad + w > capacity){
+                usedDays++;
+                currentLoad = 0;
+            }
+            
+            currentLoad += w;
+        }
+        
+        return usedDays <= days;
+    }
+}
