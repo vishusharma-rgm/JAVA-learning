cd5e3dca7d0d8059db1530f7cf3c0b86f2967a73
vishusharma <sikaridon9@gmail.com>
2026-04-13 21:39:52 +0530

    java

---
+++ CheetSheet/Leetcode/AddTwoInteger_2235.java
@@ -0,0 +1,12 @@
+package CheetSheet.Leetcode;
+
+public class AddTwoInteger_2235 {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int sum = num1 + num2;
+        System.out.println(sum);
+        
+    }
+    
+}
---
+++ CheetSheet/Leetcode/PalindromeNumber_9.java
@@ -0,0 +1,13 @@
+package CheetSheet.Leetcode;
+
+import java.util.Scanner;
+
+public class PalindromeNumber_9 {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc = new Scanner(System.in);
+    
+    }
+    
+}
