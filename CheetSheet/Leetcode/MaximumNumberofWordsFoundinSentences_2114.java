383432674dc141d8d2992d751eefc39e86090e3b
vishusharma <sikaridon9@gmail.com>
2026-04-18 22:42:49 +0530

    java

---
+++ CheetSheet/Leetcode/MaximumNumberofWordsFoundinSentences_2114.java
@@ -0,0 +1,14 @@
+package CheetSheet.Leetcode;
+
+public class MaximumNumberofWordsFoundinSentences_2114 {
+    public static void main(String[] args) {
+        String[] sentences= {"alice and bob love leetcode", 
+        "i think so too",
+        "this is great thanks very much"
+    };
+    
+    
+    }
+    
+    
+}
---
+++ CheetSheet/Leetcode/Solution.java
@@ -0,0 +1,10 @@
+package CheetSheet.Leetcode;
+
+public class Solution {
+
+    public char[] mostWordsFound(String[] sentences) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostWordsFound'");
+    }
+
+}
