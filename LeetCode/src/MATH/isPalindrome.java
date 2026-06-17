bd5681e5f4331c8c8c65e0df7cea78ac197480bb
vishusharma <sikaridon9@gmail.com>
2026-06-18 00:24:07 +0530

    JAVA

---
+++ LeetCode/src/MATH/isPalindrome.java
@@ -0,0 +1,23 @@
+package MATH;
+
+public class isPalindrome {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int original = x;
+        int reversed = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
+    }
+
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(isPalindrome(x)); // Output: true
+    }
+    
+}
