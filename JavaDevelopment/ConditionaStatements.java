8aef405bab911522963c3eeaaa2791384b6611ee
vishusharma <sikaridon9@gmail.com>
2026-06-18 19:23:42 +0530

    JAVA DEVELOPMENT

---
+++ JavaDevelopment/ConditionaStatements.java
@@ -0,0 +1,33 @@
+package JavaDevelopment;
+
+public class ConditionaStatements {
+    public static void main(String[] args) {
+
+         int x = 10;
+        if (x > 0) {
+            System.out.println("x is positive");    
+        } else if (x < 0) {
+            System.out.println("x is negative");
+        } else {
+            System.out.println("x is zero");
+
+        // int x = 2;
+        // int y = 5;
+        // int a =31;
+        // int b = 3;
+
+        // if(x<y){
+        //     System.out.println("y is greater than x");
+        // }else if (a>b){
+        //     System.out.println("a is greater than b");
+        // }else{
+        //     System.out.println("a equals b");
+        // }
+        }
+
+    }
+}
+
+     
+    
+
---
+++ JavaDevelopment/LogicalOperator.java
@@ -0,0 +1,20 @@
+package JavaDevelopment;
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+        
+    
+    int x = 10;
+    int y = 20; 
+    int a = 5;
+    int b = 4;
+
+    // boolean result = x>y && a<b;
+    // boolean result = x>y || a<b;
+      boolean result =  a<b;
+        System.out.println(!result);
+
+
+    // System.out.println(result); 
+    }
+}
