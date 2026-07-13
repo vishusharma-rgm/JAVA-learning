64af08bf775e31fe68177210ffec0f1e3f048668
vishusharma <sikaridon9@gmail.com>
2026-07-13 21:03:03 +0530

    CodeForce

---
+++ CodeForce/Implementation/The67th_IntegerProblem2218A.java
@@ -0,0 +1,29 @@
+// https://codeforces.com/problemset/problem/2218/A
+package CodeForce.Implementation;
+
+import java.io.*;
+import java.util.*;
+
+public class The67th_IntegerProblem2218A {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        StringBuilder sb = new StringBuilder();
+
+        while(t-->0){
+            int x = Integer.parseInt(br.readLine().trim());
+            sb.append(67).append('\n');
+
+
+            
+        }
+        System.out.println(sb);
+        
+
+        
+
+        
+    }
+    
+}
